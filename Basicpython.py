95999b6377b834f4e63d89bfbaea1bee5a2fb0bc
ranjithkumar <ranjithkumar.khan@gmail.com>
2022-12-06 23:50:18 +0530

    learning basic python program

---
+++ Basicpython.py
@@ -0,0 +1,67 @@
+print("welcome to python", "my name is Ranjtih","age", 30)
+print(50+10)
+print(100>15)
+print(100>100)
+print("-------------------------------")
+
+#Variables defining
+
+a=10 
+b=20
+print(a, b)
+print(id(a), id(b)) #getting the memory of a and b variable
+print("-------------------------------")
+#Arithmetic operators
+c=20
+d=30
+print(c+d)
+print (c-d)
+print(c/d)
+print(c*d)
+print(d%4)
+print(4**4)
+print(5**2)
+print (c//3)
+print("************Assignment operator************************")
+#Assginment operator
+x=10
+print(x)
+
+x=x+20
+print(x)
+
+x+=30
+print(x)
+
+y=20
+print(y)
+
+y=y-10
+print(y)
+
+y-=2
+print(y)
+
+print("***********Comparing operator******")
+
+a=90
+b=30
+print(a==b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+print ("********Logical Operator*****and,or,not")
+
+x=5
+y=40
+z=50
+print(x>y and x>z)
+
+print(x>y or z>x)
+
+print(not(y>x and z<x))
+
+
+ 
